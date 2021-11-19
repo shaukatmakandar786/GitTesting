1c3a9a9870edf9ec219386c271f45f702c8cc507
Shaukat Makandar <shaukatmakandar786@gmail.com>
2021-11-19 15:58:16 +0530

    added some ...........!

--- Multiplay.java
+++ Multiplay.java
@@ -2,6 +2,6 @@ public class Multiplay {
    
     public static void main(String[] args) {
         
-        System.out.println("Multiplication");
+        System.out.println("Multiplication...........!");
     }
 }
